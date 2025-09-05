fa91de07ee80d599216bebde8c90ac70715ad75f
anirudh <anirudhdevs91@gmail.com>
2025-09-05 07:20:47 +0530

    dsa

--- basic_math/prime.py
+++ basic_math/prime.py
@@ -6,4 +6,13 @@ def isprime(n):
             return False
     return True
     
-print(isprime(2))  
+print(isprime(2)) #True
+print(isprime(4)) #False
+print(isprime(7)) #True
+print(isprime(9)) #False
+print(isprime(11)) #True
+print(isprime(13)) #True
+print(isprime(15)) #False
+print(isprime(17)) #True
+print(isprime(19)) #True
+print(isprime(21)) #False
