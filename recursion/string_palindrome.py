280d00f4765ad83248653012f10339ec0cc7f95e
anirudh <anirudhdevs91@gmail.com>
2025-09-07 22:44:41 +0530

    recursion-partial-2

---
+++ recursion/string_palindrome.py
@@ -0,0 +1,12 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        filtered = [c.lower() for c in s if c.isalnum()]
+        
+        def helper(l, r):
+            if l >= r:  
+                return True
+            if filtered[l] != filtered[r]:
+                return False
+            return helper(l + 1, r - 1) 
+
+        return helper(0, len(filtered) - 1)
