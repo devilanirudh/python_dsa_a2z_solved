8d2e21283dc37833b99be09d79b6982dffd40234
anirudh <anirudhdevs91@gmail.com>
2025-10-12 12:56:30 +0530

    longest subarray

--- arrays/longest_subarry_length.py
+++ arrays/longest_subarry_length.py
@@ -4,18 +4,21 @@ def longest_subarray(n,k):
     length = 0
     for i in range(len(n)):
         prefix_sum = n[i]+prefix_sum
+        if k == prefix_sum:
+            length = i+1
         to_check=prefix_sum-k
         if to_check in hash_map:
             f = hash_map.get(to_check)
             l = i-f
             if length < l:
                 length = l
-        hash_map.update({prefix_sum:i})
+        if prefix_sum not in hash_map:
+            hash_map.update({prefix_sum:i})
         if k == prefix_sum:
             length = i+1
     return length
 
 
-n= [1,2,3,1,1,1,1,4,2,3] 
+n= [1,2,0,0,0,0,0,3,1,1,1,1,4,2,3] 
 k=3
 print(longest_subarray(n,k))
