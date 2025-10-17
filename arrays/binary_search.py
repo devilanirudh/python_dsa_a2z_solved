ae7c3fbde1ada43b6a30691f30da5274eaa4984a
anirudh <anirudhdevs91@gmail.com>
2025-10-17 05:51:06 +0530

    binary search recursive

--- arrays/binary_search.py
+++ arrays/binary_search.py
@@ -1,3 +1,6 @@
+from sys import implementation
+
+
 class Solution:
     def search(self, nums: List[int], target: int) -> int:
         i = 0
@@ -9,3 +12,21 @@ class Solution:
             else:
                 j = mid-1
         return -1
+
+
+# method2
+
+# recursive implementation
+# def binary_search_recursion(n,low,high,target):
+#     if low>high:
+#         return -1
+#     mid = (low+high)//2
+#     if n[mid] == target:
+#         return mid
+#     elif target>n[mid]:
+#         return binary_search_recursion(n,mid+1,high,target)
+#     return binary_search_recursion(n,low,mid-1,target)
+    
+# a = [1,2,3,4,5]
+# print(binary_search_recursion(a,0,4,4))
+        
