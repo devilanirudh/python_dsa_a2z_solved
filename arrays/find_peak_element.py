2e603399503aaba813accd4bf9061f818511da4b
anirudh <anirudhdevs91@gmail.com>
2025-10-22 11:52:18 +0530

    find peak element

---
+++ arrays/find_peak_element.py
@@ -0,0 +1,16 @@
+class Solution:
+    def findPeakElement(self, nums: List[int]) -> int:
+        i = 1
+        j = len(nums)-2
+        n = len(nums)
+        if len(nums)==1:return 0
+        if nums[0]>nums[1]:return 0
+        if nums[n-1]>nums[n-2]:return n-1
+        while(i<=j):
+            mid = (i+j)//2
+            if (nums[mid-1]<nums[mid] and nums[mid]>nums[mid+1]):return mid
+            elif (nums[mid-1]<nums[mid]):i=mid+1
+            else:j = mid-1
+        return -1
+        
+        
