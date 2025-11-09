56a7aae352488e9b3b67ea8c9768796537a9f4a2
anirudh <anirudhdevs91@gmail.com>
2025-11-09 10:18:25 +0530

    subsets backtracking

--- advanced_recursion/subsets.py
+++ advanced_recursion/subsets.py
@@ -4,4 +4,35 @@ class Solution:
         for num in nums:
             a += [subset + [num] for subset in a]
         return a
+
+
+# bitmask method
+
+class Solution:
+    def subsets(self, nums: List[int]) -> List[List[int]]:
+        n = len(nums)
+        a = []
+        for i in range(pow(2,n)):
+            sub=[]
+            for j in range(n):
+                if(i&(1<<j)):
+                    sub.append(nums[j])
+            a.append(sub)
+        return a
         
+
+#using backtracking
+
+class Solution:
+    def subsets(self, nums: List[int]) -> List[List[int]]:
+        result = []
+        def backtrack(i,a):
+            if i==len(nums):
+                result.append(a[:])
+                return 
+            a.append(nums[i])
+            backtrack(i+1,a)
+            a.pop()
+            backtrack(i+1,a)
+        backtrack(0,[])
+        return result
